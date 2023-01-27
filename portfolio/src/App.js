40bb2376a83d5d45a8da688503ee4b5bd611bb5f
mattmald <Mat.maldonado012@gmail.com>
2023-01-26 18:31:14 -0600

    started the app

---
+++ portfolio/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LandingPage from './components/LandingPage';
+import  Contact  from './components/Contact';
+
+export default function App() {
+  return (
+      <div>
+  <nav>this is my header</nav>
+      <LandingPage />
+    </div>
+  );
+}
---
+++ portfolio/src/components/LandingPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function LandingPage() {
+  return (
+    <div className="container">
+      <h1>Hello students!</h1>
+      <p>Today we will be learning about the following:</p>
+      <ul className="parent">
+        <li>React</li>
+        <li>Components</li>
+        <li>Props</li>
+        <li>Babel</li>
+      </ul>
+    </div>
+  );
+}
