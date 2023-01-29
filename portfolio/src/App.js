8cb0e84e39eae194219c4409f94192859d63eafa
mattmald <Mat.maldonado012@gmail.com>
2023-01-29 17:32:04 -0600

    started pages

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import LandingPage from './components/LandingPage';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
+
 
 
 
 export default function App() {
   return (
     <div>
-      <Navbar />
       <LandingPage />
-      <Footer />
     </div>
   );
 }
--- portfolio/src/components/LandingPage.js
+++ portfolio/src/components/LandingPage.js
@@ -1,29 +1,37 @@
 import React from 'react';
 
 export default function LandingPage() {
-    const BorderStyle = { border: '1px black', padding: '5px' };
+    const BorderStyle = {
+        background: 'gary',
+        padding: '50px',
+        border: '1px black',
+    };
 
     return (
-        <nav>
-            <section
-                style={{
-                    background: "blue",
-                    display: 'flex'
-                }}
-            >
-                <div style={BorderStyle}>
-                    <a href='https://www.youtube.com/'>About Me</a>
-                </div>
-                <div style={BorderStyle}>
-                    <a href='https://www.youtube.com/'>Portfolio</a>
-                </div>
-                <div style={BorderStyle}>
-                    <a href='https://www.youtube.com/'>Resume</a>
-                </div>
-                <div style={BorderStyle}>
-                    <a href='https://www.youtube.com/'>Contact Me</a>
-                </div>
-            </section>
-        </nav>
+        <div>
+            <nav>
+                <section
+                    style={{
+                        background: 'gray',
+                        display: 'flex',
+                        justifyContent: 'space-around',
+                        justifyItems: 'center',
+                    }}
+                >
+                    <div>
+                        <a href='https://www.youtube.com/'>About Me</a>
+                    </div>
+                    <div>
+                        <a href='https://www.youtube.com/'>Portfolio</a>
+                    </div>
+                    <div>
+                        <a href='https://www.youtube.com/'>Resume</a>
+                    </div>
+                    <div>
+                        <a href='https://www.youtube.com/'>Contact Me</a>
+                    </div>
+                </section>
+            </nav>
+        </div>
     );
 }
